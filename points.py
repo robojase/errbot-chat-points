e714cad29dc74813d26cbb2ee950783ae28160d7
Unknown <cleric.ineum@gmail.com>
2017-03-25 17:26:35 -0400

    Add configuration options to plugin

--- points.py
+++ points.py
@@ -2,8 +2,28 @@
 
 from errbot import BotPlugin, botcmd
 
+from itertools import chain
+
+CONFIG_TEMPLATE = {
+    'ROOM': 'changeme',
+    'POINTS_TIMER_S': 60,
+    'POINTS_PER_TICK': 5
+}
+
 class PointsPlugin(BotPlugin):
     """Plugin for tracking user points"""
 
     def callback_presence(self, presence):
         pass
+    def return_configuration_template(self):
+        """Returns default configuration template"""
+        return CONFIG_TEMPLATE
+
+    def configure(self, configuration):
+        """Function to process configuration setting"""
+        if configuration is not None and configuration != {}:
+            config = dict(chain(CONFIG_TEMPLATE.items(),
+                                configuration.items()))
+        else:
+            config = CONFIG_TEMPLATE
+        super(PluginExample, self).configure(config)
