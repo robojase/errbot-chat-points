9bb92067d70ad15b805e81b081495a5ab3058d2d
Unknown <cleric.ineum@gmail.com>
2017-03-25 17:58:34 -0400

    Make UserPoints an dict of UserInfo

--- points.py
+++ points.py
@@ -2,6 +2,7 @@
 
 from errbot import BotPlugin, botcmd
 
+from collections import defaultdict
 from itertools import chain
 
 CONFIG_TEMPLATE = {
@@ -20,9 +21,9 @@ class PointsPlugin(BotPlugin):
         if self.config['POINTS_TIMER_S'] > 0:
             self.start_poller(self.config['POINTS_TIMER_S'], self.add_timer_points)
 
-        #UserPoints maps user name to point value
-        if not 'UserPoints' in self:
-            self['UserPoints'] = {}
+        #UserInfo maps user name to point value
+        if not 'UserInfo' in self:
+            self['UserInfo'] = defaultdict(dict)
 
     @botcmd
     def points(self, mess, args):
@@ -30,35 +31,35 @@ class PointsPlugin(BotPlugin):
 
         #TODO: Make this smarter for general words
         point_plural = '{0}s'.format(self['POINT_NAME'])
-        if mess.frm.person() is not in self['UserPoints']:
+        if mess.frm.person() is not in self['UserInfo']:
             return '{0}: No {1} recorded yet'.format(mess.frm.nick(), point_plural)
 
-        pts = self['UserPoints'][mess.frm.person()]
         return '[{0}]: {1} {2} '.format(mess.frm.nick(), pts, point_plural)
+        pts = self['UserInfo'][mess.frm.person()]['pts']
 
     @arg_botcmd('user', admin_only=True)
     @arg_botcmd('value', admin_only=True)
     def points_set(self, mess, user, value):
         """Set a users points to a particular value"""
-        self['UserPoints'][user] = int(value)
+        self['UserInfo'][user]['pts'] = int(value)
 
     @arg_botcmd('user', admin_only=True)
     @arg_botcmd('value', admin_only=True)
     def points_add(self, mess, user, value):
         """Add points to a particular user"""
-        self['UserPoints'][user] += int(value)
+        self['UserInfo'][user]['pts'] += int(value)
 
     @arg_botcmd('user', admin_only=True)
     @arg_botcmd('value', admin_only=True)
     def points_remove(self, mess, user, value):
         """Add points to a particular user"""
-        self['UserPoints'][user] -= int(value)
+        self['UserInfo'][user]['pts'] -= int(value)
 
     def add_timer_points(self):
         """Add points to every user present when this function is called"""
         room = self._bot.query_room()
         for user in room.occupants():
-            self['UserPoints'][user.person()] += self.config['POINTS_PER_TICK']
+            self['UserInfo'][user.person()]['pts'] += self.config['POINTS_PER_TICK']
 
     def return_configuration_template(self):
         """Returns default configuration template"""
