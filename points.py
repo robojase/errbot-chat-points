b004f137a72204bd1c71a22748a93b5da0a2bc08
RoboJase <robojase@gmail.com>
2017-03-25 17:00:47 -0400

    Add first cut of plugin skeleton

---
+++ points.py
@@ -0,0 +1,9 @@
+#!python
+
+from errbot import BotPlugin, botcmd
+
+class PointsPlugin(BotPlugin):
+    """Plugin for tracking user points"""
+
+    def callback_presence(self, presence):
+        pass
